f5d9e2e767eb00de3eab94a8dfe728d40de9f27a
DeeB313 <murunmurun13@gmail.com>
2024-03-27 18:35:31 -0500

    cleaningg

--- BullyBooks/userprofile/urls.py
+++ BullyBooks/userprofile/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="userprofile/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', views.my_account, name='my_account'),
+    path('my-items/', views.my_items, name='my_items'),
+    path('my-items/add-items/', views.add_items, name='add_items'),
+    path('my-items/edit-items/<int:pk>/', views.edit_items, name='edit_items'),
+    path('my-items/delete-items/<int:pk>/', views.delete_items, name='delete_items'),
     path('seller/<int:pk>/', views.seller_detail, name='seller_detail'),
-
 ]
