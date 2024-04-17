b37877a5c1c506b59d38eb41e4131450450350fd
Amiya W <Whiteamiya17@gmail.com>
2024-04-17 12:40:41 -0500

    start up of compare feature

--- BullyBooks/inventory/urls.py
+++ BullyBooks/inventory/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search, name='search'),
+    path('compareitems/', views.compare, name='compare'),
     path('add-to-cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<str:product_id>', views.remove_from_cart, name='remove_from_cart'),
     path('change-quantity/<str:product_id>', views.change_quantity, name='change_quantity'),
@@ -11,6 +12,4 @@ urlpatterns = [
     path('cart/success/', views.success, name='success'),
     path('<slug:slug>/', views.category_detail, name='category_detail'),
     path('<slug:category_slug>/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('compare/', views.compare, name='compare'),
-
 ]
